c146032af270d9b7bfa75db31c953ccc294567f5
phghuy <phghuy.it@gmail.com>
2026-03-10 14:58:36 +0700

    add loading and 3 flash screen to app

---
+++ app/src/main/java/com/phghuy/calmihome/FlashActivity1.java
@@ -0,0 +1,54 @@
+package com.phghuy.calmihome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FlashActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_flash1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        LinearLayout btnskip = findViewById(R.id.skipbtn);
+        ImageView dotskip = findViewById(R.id.skipdot);
+        TextView skipline = findViewById(R.id.skipline);
+
+
+        btnskip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FlashActivity1.this, FlashActivity2.class);
+                startActivity(intent);
+            }
+        });
+        dotskip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FlashActivity1.this, FlashActivity2.class));
+            }
+        });
+        skipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FlashActivity1.this, FlashActivity2.class));
+            }
+        });
+    }
+}
