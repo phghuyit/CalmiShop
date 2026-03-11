9f2aa17bcd938b47972c8e571a1c0b6b7a57467e
phghuy <phghuy.it@gmail.com>
2026-03-11 10:04:05 +0700

    Temporary add fragment features

---
+++ app/src/main/java/com/phghuy/calmihome/MainActivity.java
@@ -0,0 +1,43 @@
+package com.phghuy.calmihome;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager2 pager2 = findViewById(R.id.viewPager2);
+
+        TablayoutAdapter adapter = new TablayoutAdapter(this);
+        pager2.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, pager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int i) {
+                switch (i){
+                    case 0:
+                        tab.setIcon(R.drawable.ic_home_24);
+                        tab.setText("Home");
+                        break;
+                    case 1:
+                        tab.setIcon(R.drawable.ic_history_24);
+                        tab.setText("History");
+                        break;
+                    case 2:
+                        tab.setIcon(R.drawable.ic_account_24);
+                        tab.setText("Account");
+                        break;
+                }
+            }
+        }).attach();
+    }
+}
---
+++ app/src/main/java/com/phghuy/calmihome/TablayoutAdapter.java
@@ -0,0 +1,35 @@
+package com.phghuy.calmihome;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.phghuy.calmihome.fragments.AccountFragment;
+import com.phghuy.calmihome.fragments.HisFragment;
+import com.phghuy.calmihome.fragments.HomeFragment;
+
+public class TablayoutAdapter extends FragmentStateAdapter {
+
+    public TablayoutAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 1:
+                return new HisFragment();
+            case 2:
+                return new AccountFragment();
+            default:
+                return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
